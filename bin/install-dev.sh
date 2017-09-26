c959716d161a1c349f6bb239bd4a0abf4ecc1d28
demmonico <dep.soft@mail.ru>
2017-09-26 18:01:09 +0300

    ADD copy config at install-dev.sh

--- bin/install-dev.sh
+++ bin/install-dev.sh
@@ -34,23 +34,23 @@ function configApache
     echo -n "Writing apache config ... ";
     (
         echo "<VirtualHost *:80>";
-        echo "    ServerName $sitename";
-        echo "    ServerAlias *.$sitename";
-        echo "    DocumentRoot /var/www/$sitename";
+        echo "    ServerName $SITENAME";
+        echo "    ServerAlias *.$SITENAME";
+        echo "    DocumentRoot ${SITE_DIR}";
         echo "    RewriteEngine On";
         echo "    ProxyPreserveHost On";
         echo "    UseCanonicalName On";
         echo "    ProxyPass / http://0.0.0.0:8080/";
         echo "    ProxyPassReverse / http://0.0.0.0:8080/";
-        echo "    ErrorLog /var/www/$sitename/log/error.log";
-        echo "    CustomLog /var/www/$sitename/log/access.log combined";
+        echo "    ErrorLog ${SITE_DIR}/log/error.log";
+        echo "    CustomLog ${SITE_DIR}/log/access.log combined";
         echo "</VirtualHost>";
-    ) | sudo tee /etc/apache2/sites-available/$sitename.conf
+    ) 2>&1 | tee /etc/apache2/sites-available/$SITENAME.conf
     echo -e "${GREEN}done${NC}";
 
     # restart apache
     echo -n "Restart apache service ... ";
-    sudo service apache2 restart
+    service apache2 restart
     echo -e "${GREEN}done${NC}";
 }
 
@@ -65,16 +65,20 @@ function configApache
 # set colors
 GREEN='\033[0;32m'
 RED='\033[0;31m'
+YELLOW='\033[1;33m'
 NC='\033[0m' # No Color
 
 # validate required params
-sitename=$1
-if [ -z "$sitename" ]; then
+SITENAME=$1
+if [ -z "$SITENAME" ]; then
     die "Website name cannot be empty";
 fi;
 if [ "$EUID" -ne 0 ]; then
     die "Please run as root";
 fi;
+
+# site dir
+SITE_DIR="/var/www/$SITENAME"
 
 
 
@@ -82,11 +86,29 @@ fi;
 # change apache config
 configApache;
 
+# setup docker-ci configs
+PROXY_CONFIG="$SITE_DIR/proxy/config.yml"
+PROXY_CONFIG_EXAMPLE="$SITE_DIR/proxy/config-example.yml"
+if [ ! -f "${PROXY_CONFIG}" ] && [ -f "${PROXY_CONFIG_EXAMPLE}" ]
+then
+    echo -n "Setting up docker-ci proxy config ... ";
+    # copy from example
+    cp ${PROXY_CONFIG_EXAMPLE} ${PROXY_CONFIG} && chown -R `stat . -c %u:%g` ${PROXY_CONFIG}
+    # remove example lines by pattern
+    sed -i "/^\s*-\sexample.com\s*$/d" /etc/hosts;
+    sed -i "/^\s*-\sexample.loc\s*$/d" /etc/hosts;
+    sed -i "/^\s*-\sexample.loc\s*$/d" /etc/hosts;
+    echo "    - $SITENAME" | tee -a ${PROXY_CONFIG};
+    echo -e "${GREEN}done${NC}";
+fi;
+
 
 
 #-----------------------------------------------------------#
 # finish
 echo "";
-echo "Now you can see your docker websites at \"http://$sitename/\"";
-echo "All done. Have a nice day :)";
+echo "Now you can see your docker websites at \"http://$SITENAME/\" and it subdomains";
+echo -e "${YELLOW}Info: ${NC} to create new project just create unique ${YELLOW}projects/PROJECT_NAME/docker-compose.yml${NC} file. For more info visit ${YELLOW}https://github.com/demmonico/docker-ci${NC}";
+echo -e "${YELLOW}Note: ${NC} to successful pulling from ${YELLOW}github.com${NC} you should copy your ${YELLOW}~/.ssh${NC} folder to ${YELLOW}config/ssh${NC} folder.";
+echo -e "${GREEN}All done. Have a nice day :)${NC}";
 echo "";
