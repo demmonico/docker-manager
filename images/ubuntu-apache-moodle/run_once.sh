9cc2225e59dd761c2ed199dfebfa4b0554f05721
demmonico <dep.soft@mail.ru>
2017-10-02 16:50:48 +0300

    ADD moodle repo var and setup cron job

--- images/ubuntu-apache-moodle/run_once.sh
+++ images/ubuntu-apache-moodle/run_once.sh
@@ -9,19 +9,34 @@ usermod -u ${HOST_USER_ID} www-data && groupmod -g ${HOST_USER_ID} www-data
 ### install moodle
 if [ ! -f "${PROJECT_DIR}/config-dist.php" ]; then
     # clone repo
-    git clone ${MOODLE_REPOSITORY} ${PROJECT_DIR} && \
+    git clone ${MOODLE_REPOSITORY} ${PROJECT_DIR}
+
+    # removed because IOMAD hasn't tags - branches only
     # get latest tag if empty
-    if [ -z "${MOODLE_TAG_VERSION}" ]; then MOODLE_TAG_VERSION="$( git describe --tags --abbrev=0 --match v3.* )"; fi && \
+    #if [ -z "${MOODLE_TAG_VERSION}" ]; then MOODLE_TAG_VERSION="$( git describe --tags --abbrev=0 --match v3.* )"; fi && \
     # checkout tag
-    cd ${PROJECT_DIR} && git checkout tags/${MOODLE_TAG_VERSION} && \
+    #cd ${PROJECT_DIR} && git checkout tags/${MOODLE_TAG_VERSION}
+
+    # checkout version
+    cd ${PROJECT_DIR}
+    if [ ! -z "${MOODLE_TAG_VERSION}" ]
+    then
+        git checkout tags/${MOODLE_TAG_VERSION}
+    elif [ ! -z "${MOODLE_BRANCH}" ]
+    then
+        git checkout -b temp origin/${MOODLE_BRANCH}
+    fi
+
     # remove git to prepare
     rm -rf "${PROJECT_DIR}/.git"
 fi
 
 
 ### config moodle
+
 # add configs
 mv -f /config.php "${PROJECT_DIR}/config.php"
+
 # config db
 if [ ! -z "${DB_HOST}" ]
 then
@@ -34,6 +49,17 @@ then
     fi
     sed -i "s/MOODLE_DB_NAME/${DB_NAME}/" "${PROJECT_DIR}/config.php"
 fi
+
+# add cron task
+USER_NAME=$( getent passwd ${HOST_USER_ID} | cut -d: -f1 )
+CRON_LINE_MARKER='\#\#\# begin \# moodle scheduler \#'
+CRON_NEW_LINES=$(
+    echo '### begin # moodle scheduler #'
+    echo '* * * * * /usr/bin/php -f /var/www/html/admin/cli/cron.php > /dev/null 2>&1'
+    echo '### end # moodle scheduler #'
+)
+CRON_LINES=$( crontab -u ${USER_NAME} -l > /dev/null 2>&1 )
+( echo ${CRON_LINES} | ( grep -q -F "${CRON_LINE_MARKER}" > /dev/null 2>&1 || echo "${CRON_NEW_LINES}" ) ) | crontab -u ${USER_NAME} -
 
 
 ### run custom script if exists
