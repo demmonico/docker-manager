34613080fd4f19cec5da71a81b065948a6eb6dde
demmonico <dep.soft@mail.ru>
2017-11-28 20:25:30 +0200

    add mariadb-v2 image

---
+++ images/ubuntu-mariadb-v2/install/run_once.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+#
+# DEPRECATED
+#
+# This file has executed after container's builds
+#
+# tech-stack: ubuntu / mariadb
+#
+# @author demmonico
+# @image ubuntu-mariadb
+# @version v1.1
+
+
+
+### init DB
+if [ ! -d /var/lib/mysql/mysql ]; then
+
+    # set permissions
+    chown mysql:mysql /var/lib/mysql
+
+    # init system tables
+    mysql_install_db --user=mysql --ldata=/var/lib/mysql/ --basedir=/usr
+
+    # Start the MySQL daemon in the background.
+    /usr/sbin/mysqld &
+    mysql_pid=$!
+
+    until mysqladmin ping >/dev/null 2>&1; do
+      echo -n "."; sleep 0.2
+    done
+
+    # Permit root login without password from outside container.
+    mysql -e "CREATE USER 'root'@'%' IDENTIFIED BY '';"
+    mysql -e "GRANT ALL ON *.* TO 'root'@'%' IDENTIFIED BY '' WITH GRANT OPTION;"
+
+    # create the default database
+    mysql -e "CREATE DATABASE ${DB_NAME};"
+
+    # import database from SQL if exists
+    FILE_IMPORT="/var/lib/mysql/${DB_NAME}.sql"
+    if [ -f ${FILE_IMPORT} ]
+    then
+        mysql ${DB_NAME} < ${FILE_IMPORT}
+    fi
+
+    # Tell the MySQL daemon to shutdown.
+    mysqladmin shutdown
+
+    # Wait for the MySQL daemon to exit.
+    wait $mysql_pid
+
+fi
+
+
+
+### run custom script if exists
+if [ ! -z ${CUSTOM_RUN_SCRIPT_ONCE} ] && [ -f ${CUSTOM_RUN_SCRIPT_ONCE} ] && [ -x ${CUSTOM_RUN_SCRIPT_ONCE} ]
+then
+    /bin/bash ${CUSTOM_RUN_SCRIPT_ONCE}
+fi
