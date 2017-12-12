68a19608926d42eb5b9194a07325573e80108c8b
demmonico <dep.soft@mail.ru>
2017-12-12 14:09:26 +0200

    ADD docker install at local env; FIX updating docker permissions

--- bin/install.sh
+++ bin/install.sh
@@ -122,13 +122,15 @@ fi;
 
 ### prepare environment
 
+isLocalEnv="$([ ! -z "$( netstat -ln | grep ":80 " )" ] && [ ! -z "$( dpkg --get-selections | grep apache )" ] && echo 'true' )";
+
 #-----------------------------------------------------------#
 # local environment
 # update Apache config if it is installed and is listen default port 80
 # (preferable use on the development environment: Apache at host machine + Docker Manager inside Apache as virtual host)
-if [ ! -z "$( netstat -ln | grep ":80 " )" ] && [ ! -z "$( dpkg --get-selections | grep apache )" ]; then
+if [ ! -z "${isLocalEnv}" ]; then
 
-    # check placement
+    # check for www placement
     if [ "${DM_ROOT_DIR}" == "/var/www/${HOST_NAME}" ]; then
         echo -e "${YELLOW}Info:${NC} web-server Apache detected. Start configure Apache to make website available";
 
@@ -187,44 +189,57 @@ if [ ! -z "$( netstat -ln | grep ":80 " )" ] && [ ! -z "$( dpkg --get-selections
         echo -e "${GREEN}done${NC}";
     fi
 
+fi
+
+#-----------------------------------------------------------#
+
+
+
+### install software
+
+VERSION_DOCKER_COMPOSE=${VERSION_DOCKER_COMPOSE:-'1.17.0'}
+echo -e "${YELLOW}Info:${NC} updating repositories ... "
+sudo apt-get update
+echo -e "${YELLOW}Info:${NC} install software ... "
+{ command -v docker > /dev/null 2>&1 && echo -e "${YELLOW}Info:${NC} ${GREEN}docker${NC} is already installed"; } || \
+    { \
+        # setup repository
+        sudo apt-get -y install apt-transport-https \
+            ca-certificates \
+            curl \
+            gnupg2 \
+            software-properties-common && \
+        curl -fsSL https://download.docker.com/linux/$(. /etc/os-release; echo "$ID")/gpg | sudo apt-key add - && \
+        sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/$(. /etc/os-release; echo "$ID") $(lsb_release -cs) stable" && \
+        # install docker
+        sudo apt-get update && \
+        sudo apt-get -y install docker-ce && \
+        # FIX error Docker daemon connection
+        { \
+            # 1. Create the docker group.
+            sudo groupadd docker; \
+            # 2. Add your user to the docker group.
+            sudo usermod -aG docker ${USER}; \
+            # 3. Log out and log back in so that your group membership is re-evaluated.
+            # TODO improve this
+            #exec sudo su -l ${USER} </dev/null >/dev/null 2>&1; \
+        } \
+    } && \
+{ command -v docker-compose > /dev/null 2>&1 && echo -e "${YELLOW}Info:${NC} ${GREEN}docker-compose${NC} is already installed"; } || { \
+    sudo curl -L https://github.com/docker/compose/releases/download/${VERSION_DOCKER_COMPOSE}/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose && \
+    sudo chmod +x /usr/local/bin/docker-compose; \
+}
+
+
+
 #-----------------------------------------------------------#
 # server environment
-else
 
-    echo -e "${YELLOW}Info:${NC} updating repo info ... "
-    VERSION_DOCKER_COMPOSE='1.17.0'
-    sudo apt-get update && \
-        echo -e "${YELLOW}Info:${NC} check for installs ... " && \
-        { command -v mc > /dev/null 2>&1 && echo -e "${YELLOW}Info:${NC} ${GREEN}mc${NC} is already installed"; } || \
-            sudo apt-get -y install mc && \
-        { command -v ssh > /dev/null 2>&1 && echo -e "${YELLOW}Info:${NC} ${GREEN}openssh-client${NC} is already installed"; } || \
-            sudo apt-get -y install openssh-client && \
-        { command -v docker > /dev/null 2>&1 && echo -e "${YELLOW}Info:${NC} ${GREEN}docker${NC} is already installed"; } || \
-            { \
-                # setup repository
-                sudo apt-get -y install apt-transport-https \
-                    ca-certificates \
-                    curl \
-                    gnupg2 \
-                    software-properties-common && \
-                curl -fsSL https://download.docker.com/linux/$(. /etc/os-release; echo "$ID")/gpg | sudo apt-key add - && \
-                sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/$(. /etc/os-release; echo "$ID") $(lsb_release -cs) stable" && \
-                # install docker
-                sudo apt-get update && \
-                sudo apt-get -y install docker-ce && \
-                # FIX error Docker daemon connection
-                # TODO improve this fix
-                # 1. Create the docker group.
-                sudo groupadd docker && \
-                # 2. Add your user to the docker group.
-                sudo usermod -aG docker ${USER} && \
-                # 3. Log out and log back in so that your group membership is re-evaluated.
-                exec sudo su -l ${USER} </dev/null >/dev/null 2>&1; \
-            } && \
-        { command -v docker-compose > /dev/null 2>&1 && echo -e "${YELLOW}Info:${NC} ${GREEN}docker-compose${NC} is already installed"; } || { \
-            sudo curl -L https://github.com/docker/compose/releases/download/${VERSION_DOCKER_COMPOSE}/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose && \
-            sudo chmod +x /usr/local/bin/docker-compose; \
-        }
+if [ -z "${isLocalEnv}" ]; then
+    { command -v mc > /dev/null 2>&1 && echo -e "${YELLOW}Info:${NC} ${GREEN}mc${NC} is already installed"; } || \
+        sudo apt-get -y install mc && \
+    { command -v ssh > /dev/null 2>&1 && echo -e "${YELLOW}Info:${NC} ${GREEN}openssh-client${NC} is already installed"; } || \
+        sudo apt-get -y install openssh-client
 fi
 
 #-----------------------------------------------------------#
@@ -234,7 +249,7 @@ fi
 # finish
 echo "";
 echo -e "${GREEN}All done.${NC}";
-echo -e "${GREEN}Now you can see your projects from Docker Manager as${NC} ${YELLOW}http://${HOST_NAME}/${NC} ${GREEN}and sub-domains${NC}";
+echo -e "${GREEN}Now you${NC} ${YELLOW}should logout${NC} ${GREEN}and then${NC} ${YELLOW}after login${NC} ${GREEN}you could see your projects from Docker Manager as${NC} ${YELLOW}http://${HOST_NAME}/${NC} ${GREEN}and sub-domains${NC}";
 echo -e "${YELLOW}Info: ${NC} to create new project just create unique ${YELLOW}projects/PROJECT_NAME/docker-compose.yml${NC} file. For more info visit ${YELLOW}https://github.com/demmonico/docker-manager${NC}";
 echo -e "${GREEN}Have a nice day :)${NC}";
 echo "";
